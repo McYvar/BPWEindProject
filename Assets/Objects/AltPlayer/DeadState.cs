a0a79b42ff8a8434d428d867b9d9e9cad284e413
McYvar <yvarftoorop@hotmail.com>
2021-12-14 01:17:51 +0100

    Fancy Gun Added

---
+++ Assets/Objects/AltPlayer/DeadState.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadState : BaseState
+{
+    Player player;
+    Rigidbody rb;
+    public override void OnAwake()
+    {
+        player = GetComponent<Player>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public override void OnEnter()
+    {
+        player.transform.localScale = new Vector3(player.transform.localScale.x, player.transform.localScale.y / 2, player.transform.localScale.z);
+
+        rb.velocity = new Vector3(0, rb.velocity.y, 0);
+
+        player.Dead();
+    }
+
+    public override void OnExit()
+    {
+    }
+
+    public override void OnFixedUpdate()
+    {
+    }
+
+    public override void OnUpdate()
+    {
+    }
+}
